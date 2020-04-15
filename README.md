1f0c505f468da66fa61d684620c20830986694fc
Malnati <ricardomalnati@gmail.com>
2020-04-14 21:59:17 -0300

    Adding /data end-point.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mal-poc-njs-chartjs
 A proof of concept for using Chart JS over node js.
+
+# Usage
+npm install && node server.js
--- server.js
+++ server.js
@@ -10,6 +10,29 @@ app.get('/', async (request, response) => {
     await response.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/data', async (request, response) => {
+  const data = {
+    datasets: [{
+            data: [10, 20, 30, 20],
+            backgroundColor: [
+                '#ff6384',
+                '#36a2eb',
+                '#cc65fe',
+                '#ffce56'
+            ]
+        }],
+    labels: [
+            'Red',
+            'Blue',
+            'Purple',
+            'Yellow'
+        ]
+    };
+  let msg = JSON.stringify(data);
+  response.status(200).send(msg);
+  response.status(200).end();
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.info(`Server started on port: ${process.env.PORT || 3000}`);
 });
