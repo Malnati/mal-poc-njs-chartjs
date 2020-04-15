155cb819784e72d9c02093f40c2b8e98a28371b5
Malnati <ricardomalnati@gmail.com>
2020-04-14 21:49:43 -0300

    Removing unecessary code.

--- server.js
+++ server.js
@@ -3,27 +3,11 @@ const path = require('path');
 const chart = require('chart.js');
 
 const app = express();
-
-var options = {
-    dotfiles: 'ignore',
-    etag: false,
-    extensions: ['htm', 'html'],
-    index: false,
-    maxAge: '1d',
-    redirect: false,
-    setHeaders: function (res, path, stat) {
-      res.set('x-timestamp', Date.now());
-    }
-  };
   
 app.use("/public", express.static('./public/'));  
 
 app.get('/', async (request, response) => {
     await response.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get('/doughnut', async (request, response) => {
-    await response.sendFile(path.join(__dirname + '/doughnut.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
