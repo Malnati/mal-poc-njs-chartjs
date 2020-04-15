968866200bbd268a454ccbdd03cc4803c93c0a87
Malnati <ricardomalnati@gmail.com>
2020-04-14 21:00:59 -0300

    Fixing public diretcory path.

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-var Chart = require('chart.js');
-// var myChart = new Chart(ctx, {...});
+const express = require('express');
+const path = require('path');
+const chart = require('chart.js');
 
-// let myChart = new Chart(new CanvasRenderingContext2D());
+const app = express();
+
+var options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+      res.set('x-timestamp', Date.now());
+    }
+  };
+  
+app.use("/public", express.static('./public/'));  
+
+app.get('/', async (request, response) => {
+    await response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/doughnut', async (request, response) => {
+    await response.sendFile(path.join(__dirname + '/doughnut.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.info(`Server started on port: ${process.env.PORT || 3000}`);
+});
